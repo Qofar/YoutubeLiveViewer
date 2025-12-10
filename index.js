b965cb97484ce850213a0428d230de8e1c86f6ea
Qofar <plakia@gmail.com>
2025-12-11 00:21:47 +0900

    first commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+const open = function(){
+    const url = document.getElementById("input_url").value;
+
+	const regex = /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?/;
+	const matches = url.match(regex);
+
+	if(matches[5]) {
+		const videoUrl = "https://www.youtube.com/embed/" + matches[5];
+		const chatUrl  = "https://www.youtube.com/live_chat?is_popout=1&v=" + matches[5];
+
+		document.getElementById("iVideo").src = videoUrl;
+		document.getElementById("iChat").src  = chatUrl;
+    }
+}
+const clear = function(){
+	document.getElementById("iVideo").src = "";
+	document.getElementById("iChat").src = "";
+}
+document.getElementById("btn_open").onclick  = open;
+document.getElementById("btn_clear").onclick = clear;
+document.getElementById("input_url").focus();
