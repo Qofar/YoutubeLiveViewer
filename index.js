dda16d80ca03c081cc6acca4f1b31cc04c060b7e
Qofar <plakia@gmail.com>
2025-12-16 22:46:18 +0900

    add embed_domain

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const open = function(){
 
 	if(matches[5]) {
 		const videoUrl = "https://www.youtube.com/embed/" + matches[5];
-		const chatUrl  = "https://www.youtube.com/live_chat?is_popout=1&v=" + matches[5];
+		const chatUrl  = "https://www.youtube.com/live_chat?is_popout=1&v=" + matches[5] + "&embed_domain=qofar.github.io";
 
 		document.getElementById("iVideo").src = videoUrl;
 		document.getElementById("iChat").src  = chatUrl;
@@ -19,3 +19,4 @@ const clear = function(){
 document.getElementById("btn_open").onclick  = open;
 document.getElementById("btn_clear").onclick = clear;
 document.getElementById("input_url").focus();
+
